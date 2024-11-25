3e39b63fb296ca326c44c74f62acd185af5a0e8e
haiyoucuv <qiuxu123456@live.com>
2024-11-25 21:01:10 +0800

    ThirdCamera

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,99 +7,108 @@ import { navLinks } from "../constants";
 import logo from "../assets/H.svg";
 import menu from "../assets/menu.svg";
 import close from "../assets/close.svg";
+import github from "../assets/icon/github.svg?raw";
+import { color } from "framer-motion";
 
 const Navbar = () => {
-    const [active, setActive] = useState("");
-    const [toggle, setToggle] = useState(false);
-    const [scrolled, setScrolled] = useState(false);
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const scrollTop = window.scrollY;
-            if (scrollTop > 100) {
-                setScrolled(true);
-            } else {
-                setScrolled(false);
-            }
-        };
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
 
-        window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
-    return (
-        <nav
-            className={`${
-                styles.paddingX
-            } w-full flex items-center py-5 fixed top-0 z-20 ${
-                scrolled ? "bg-primary" : "bg-transparent"
-            }`}
+  return (
+    <nav
+      className={`${
+        styles.paddingX
+      } w-full flex items-center py-5 fixed top-0 z-20 ${
+        scrolled ? "bg-primary" : "bg-transparent"
+      }`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
         >
-            <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-                <Link
-                    to="/"
-                    className="flex items-center gap-2"
-                    onClick={() => {
-                        setActive("");
-                        window.scrollTo(0, 0);
-                    }}
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex ">
+            Haiyoucuv &nbsp;
+            <span className="sm:block"> | Profile</span>
+          </p>
+        </Link>
+
+        <ul className="list-none hidden sm:flex flex-row gap-10 items-center">
+          {navLinks.map((nav) => (
+            <li
+              key={nav.id}
+              className={`${
+                active === nav.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(nav.title)}
+            >
+              <a href={nav.id}>{nav.title}</a>
+            </li>
+          ))}
+
+          <li className="cursor-pointer flex items-center">
+            <a href="https://github.com/haiyoucuv/profile"
+               className="fill-secondary hover:fill-white w-[50px] h-[50px]"
+               dangerouslySetInnerHTML={{ __html: github }}
+            />
+          </li>
+        </ul>
+
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain"
+            onClick={() => setToggle(!toggle)}
+          />
+
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className="list-none flex justify-end items-start flex-1 flex-col gap-4">
+              {navLinks.map((nav) => (
+                <li
+                  key={nav.id}
+                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                    active === nav.title ? "text-white" : "text-secondary"
+                  }`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(nav.title);
+                  }}
                 >
-                    <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-                    <p className="text-white text-[18px] font-bold cursor-pointer flex ">
-                        Haiyoucuv &nbsp;
-                        <span className="sm:block"> | Profile</span>
-                    </p>
-                </Link>
-
-                <ul className="list-none hidden sm:flex flex-row gap-10">
-                    {navLinks.map((nav) => (
-                        <li
-                            key={nav.id}
-                            className={`${
-                                active === nav.title ? "text-white" : "text-secondary"
-                            } hover:text-white text-[18px] font-medium cursor-pointer`}
-                            onClick={() => setActive(nav.title)}
-                        >
-                            <a href={`#${nav.id}`}>{nav.title}</a>
-                        </li>
-                    ))}
-                </ul>
-
-                <div className="sm:hidden flex flex-1 justify-end items-center">
-                    <img
-                        src={toggle ? close : menu}
-                        alt="menu"
-                        className="w-[28px] h-[28px] object-contain"
-                        onClick={() => setToggle(!toggle)}
-                    />
-
-                    <div
-                        className={`${
-                            !toggle ? "hidden" : "flex"
-                        } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
-                    >
-                        <ul className="list-none flex justify-end items-start flex-1 flex-col gap-4">
-                            {navLinks.map((nav) => (
-                                <li
-                                    key={nav.id}
-                                    className={`font-poppins font-medium cursor-pointer text-[16px] ${
-                                        active === nav.title ? "text-white" : "text-secondary"
-                                    }`}
-                                    onClick={() => {
-                                        setToggle(!toggle);
-                                        setActive(nav.title);
-                                    }}
-                                >
-                                    <a href={`#${nav.id}`}>{nav.title}</a>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    );
+                  <a href={nav.id}>{nav.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { createRoot } from "react-dom/client";
 
 // import "@csstools/normalize.css";
 
+import './i18n';
+
 import "./index.less";
 import App from "./App.tsx";
 import { StrictMode } from "react";
